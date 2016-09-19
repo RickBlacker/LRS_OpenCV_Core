cd1d656f523bba68a1ed88d4c92f76bd9f291756
Rick <rick_blacker@hotmail.com>
2016-09-19 14:17:54 -0700

    Add files via upload

---
+++ objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lrealsense -lopencv_core -lopencv_imgcodecs -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_video -lopencv_flann
+
